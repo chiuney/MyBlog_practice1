b3a64d9b6cccd6de58c9ea59b0e7f1fa9b701a57
1vcpu <1virtualcpu@gmail.com>
2020-09-25 19:19:06 +0900

    modify post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,11 +13,21 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def create
     @post = Post.new(post_params)
     if @post.save
+      redirect_to @post
+    else
+      render "posts/new"
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
       redirect_to @post
     else
       render "posts/new"
