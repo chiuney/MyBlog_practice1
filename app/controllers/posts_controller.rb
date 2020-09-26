0111d8f26783c7944ecfe067d3fc11a678ec1882
chiuney <yokota.chiune@gmail.com>
2020-09-26 00:14:00 +0000

    Initialize repository

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,33 @@
+class PostsController < ApplicationController
+  
+  def new
+    @post = Post.new
+  end
+
+  def index
+    @post = Post.all.order(id: "DESC")
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render "posts/new"
+    end
+  end
+
+    private
+
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
+
+end
