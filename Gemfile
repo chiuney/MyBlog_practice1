ea22169f41bfa2b9184d4e00a9077d6670d6e756
chiuney <yokota.chiune@gmail.com>
2020-09-29 14:20:22 +0900

    install rails_admin

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.7.1'
 
 # Add admin
-gem 'activeadmin'
+gem 'rails_admin', '~> 2.0.0.beta'
 gem 'devise'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.3'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_28_020608) do
+ActiveRecord::Schema.define(version: 2020_09_29_050906) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
